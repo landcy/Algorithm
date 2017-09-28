4e35c70c79e2550d9e39b142160a3e68749bc3e9
landcy <landon_sky@163.com>
2017-09-28 17:55:42 +0800

    generate parentheses in leet code 22

---
+++ src/String/GenerateParentheses22.java
@@ -0,0 +1,41 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+public class GenerateParentheses22 {
+	public static List<String> generateParenthesis(int n) {
+		HashSet<String> set = generateOneByOne(n);
+		List<String> result = new ArrayList<String>(set);
+		return result;
+	}
+
+	private static HashSet<String> generateOneByOne(int n) {
+		if (n <= 0) {
+			return null;
+		}
+		HashSet<String> result = new HashSet<String>();
+		if (n == 1) {
+			result.add("()");
+			return result;
+		}
+		HashSet<String> set = generateOneByOne(n - 1);
+		Iterator<String> iterator = set.iterator();
+		while (iterator.hasNext()) {
+			String str = iterator.next();
+			result.add("()" + str);
+			result.add(str + "()");
+			result.add("(" + str + ")");
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		List<String> result = generateParenthesis(1);
+		for (String str : result) {
+			System.out.println(str);
+		}
+	}
+}
